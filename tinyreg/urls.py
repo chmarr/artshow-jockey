c0958eeb65fd9d7bab66b0febb3f91fa8b974a94
Chris Cogdon <chris@cogdon.org>
2012-11-30 13:26:51 -0800

    Added Captcha to Password Recovery screen

--- tinyreg/urls.py
+++ tinyreg/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from tinyreg.forms import CaptchaPasswordResetForm
 
 urlpatterns = patterns('',
     url ( r'login/$', 'django.contrib.auth.views.login', {'template_name':'accounts/login.html'}, name="login" ),
@@ -6,7 +7,12 @@ urlpatterns = patterns('',
     url ( r'password_change/$', 'django.contrib.auth.views.password_change', {'template_name':'accounts/password_change_form.html'}, name="password_change" ),
     url ( r'password_change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name':'accounts/password_change_done.html'}, name="password_change_done" ),
     url ( r'password_reset/$', 'django.contrib.auth.views.password_reset', 
-    		{ 'template_name':'accounts/password_reset_form.html','email_template_name':'accounts/password_reset_email.html','subject_template_name':'accounts/password_reset_subject.txt'},
+    		{ 
+    			'template_name':'accounts/password_reset_form.html',
+    			'email_template_name':'accounts/password_reset_email.html',
+    			'subject_template_name':'accounts/password_reset_subject.txt',
+    			'password_reset_form':CaptchaPasswordResetForm,
+    		},
     		name="password_reset" ),
     url ( r'password_reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name':'accounts/password_reset_done.html'}, name="password_reset_done" ),
     url ( r'reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name':'accounts/password_reset_confirm.html'}, name="password_reset_confirm" ),

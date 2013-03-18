cfad026cf4dd8e30a817dadf8fc110ea8692c609
Chris Cogdon <chris@cogdon.org>
2013-03-17 17:21:32 -0700

    Added tests for AttributeFilter (Holy crap, this is the first test being added!)

--- artshow/tests.py
+++ artshow/tests.py
@@ -1,24 +1,17 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from artshow.utils import artshow_settings
+from django.conf import settings
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class AttributeFilterTest (TestCase):
+    def test_passed_access(self):
+        self.assertEqual(artshow_settings.SITE_NAME, settings.SITE_NAME)
+        self.assertEqual(artshow_settings.SITE_ROOT_URL, settings.SITE_ROOT_URL)
+        self.assertEqual(artshow_settings.ARTSHOW_SHOW_YEAR, settings.ARTSHOW_SHOW_YEAR)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_failed_access(self):
+        def get_secret_key():
+            return artshow_settings.SECRET_KEY
+        self.assertRaisesRegexp(AttributeError,
+                                "AttributeFilter blocked access to 'SECRET_KEY'.*",
+                                get_secret_key)

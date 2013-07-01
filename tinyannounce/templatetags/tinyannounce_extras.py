572076cee4d64c4b77e724010f8a38ba819a2e6b
Chris Cogdon <chris@cogdon.org>
2013-07-01 00:15:56 -0700

    Merge branch 'master' of github.com:chmarr/artshow-jockey
    Conflicts:
    	artshow/default_settings.py

--- tinyannounce/templatetags/tinyannounce_extras.py
+++ tinyannounce/templatetags/tinyannounce_extras.py
@@ -9,11 +9,12 @@ def announcements_available(context):
     user = context['request'].user
     announcements = Announcement.objects.active()
     total = new = new_and_important = 0
-    for a in announcements:
-        total += 1
-        if not a.is_seen_by(user):
-            new += 1
-            if a.important:
-                new_and_important += 1
+    if user and user.is_authenticated():
+        for a in announcements:
+            total += 1
+            if not a.is_seen_by(user):
+                new += 1
+                if a.important:
+                    new_and_important += 1
     return {'announcements': announcements, 'total': total, 'new': new, 'new_and_important': new_and_important}
 

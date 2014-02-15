e2215c40e8d2bed81909e76c03cf35858ba6fe86
Chris Cogdon <chris@cogdon.org>
2014-02-15 14:36:47 -0800

    Adding Reg DB integration.

--- artshow/unicodewriter.py
+++ artshow/unicodewriter.py
@@ -75,3 +75,28 @@ class UnicodeDictWriter(UnicodeWriter):
     def writerow(self, drow):
         row = [drow.get(field, '') for field in self.fields]
         super(UnicodeDictWriter, self).writerow(row)
+
+
+class UnicodeReader(object):
+    def __init__(self, f, encoding="utf-8", **kwargs):
+        self.csv_reader = csv.reader(f, **kwargs)
+        self.encoding = encoding
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        # read and split the csv row into fields
+        row = self.csv_reader.next()
+        # now decode
+        return [unicode(cell, self.encoding) for cell in row]
+
+    @property
+    def line_num(self):
+        return self.csv_reader.line_num
+
+
+class UnicodeDictReader(csv.DictReader):
+    def __init__(self, f, encoding="utf-8", fieldnames=None, **kwds):
+        csv.DictReader.__init__(self, f, fieldnames=fieldnames, **kwds)
+        self.reader = UnicodeReader(f, encoding=encoding, **kwds)

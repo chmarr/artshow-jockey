ff733d13da89d5cfc2d9a1e3c360107d14fde2b8
Chris Cogdon <chris@cogdon.org>
2016-01-30 18:17:48 -0800

    Adjustments for person import

--- peeps/management/commands/importpeeps.py
+++ peeps/management/commands/importpeeps.py
@@ -86,17 +86,15 @@ class Command(BaseCommand):
     targets = ("name", "address1", "address2", "city", "state", "country",
                "postcode", "phone", "email", "reg_id", "comment")
 
-    map_re = re.compile("(?P<source>[\w ]+)=(?P<target>\w+)$")
+    map_re = re.compile("(?P<source>[^=]+)=(?P<target>\w+)$")
 
     option_list = BaseCommand.option_list + (
         make_option("--map", type="string", action="append", default=default_map,
                     help="add a new map. sourcename=targetname."),
         make_option("--map-reset", action="callback", callback=map_reset, help="clear the map"),
-        make_option("--comment", type="string", default="", help="add this comment"),
         make_option("-n", "--dry-run", action="store_true", default=False, help="show results, Don't update DB"),
-        make_option("-o", "--results", type="string", default=None, help="write results to file"),
-        make_option("-i", "--input", type="string", default=None,
-                    help="read previous results to assist in decisions from file"),
+        make_option("--skip-missing-reg-id", action="store_true", default=False,
+                    help="Skip persons with a missing Reg ID"),
         make_option("--dialect", type="string", default="excel", help="csv dialect [%default]"),
         make_option("--encoding", type="string", default="utf-8", help="encoding [%default]"),
     )
@@ -106,12 +104,7 @@ class Command(BaseCommand):
         mapping = self.convert_mapping(options['map'])
         db = self.read_reg_csv(args[0], mapping, options['dialect'], options['encoding'])
 
-        if options['results']:
-            results = open(options['results'], "w")
-        else:
-            results = None
-
-        self.merge_peeps(db, dry_run=options['dry_run'], results=results, comment=options['comment'])
+        self.merge_peeps(db, dry_run=options['dry_run'], skip_no_regid=options['skip_missing_reg_id'])
 
     def print_person(self, person):
         print "Person ID:%s  Name:%s  RegID:%s  Comment:%s" % (person.id, person.name, person.reg_id, person.comment)
@@ -129,18 +122,20 @@ class Command(BaseCommand):
                    postcode=result[6], phone=result[7], email=result[8], reg_id=result[9], comment=result[10])
         return p
 
-    def merge_peeps(self, db, dry_run=False, results=None, comment=None):
+    def merge_peeps(self, db, dry_run=False, skip_no_regid=False):
 
         print
 
         for person in Person.objects.all():
 
             if person.reg_id == "":
+                if skip_no_regid:
+                    continue
                 print "Missing Reg ID for:"
                 self.print_person_summary(person)
                 csv_person = None
             else:
-                results = db.execute("select * from person where reg_id=?", (person.reg_id, ))
+                results = db.execute("select * from person where reg_id=?", (person.reg_id,))
                 result = results.fetchone()
                 if result:
                     csv_person = self.row_into_person(result)
@@ -150,7 +145,7 @@ class Command(BaseCommand):
                     csv_person = None
 
             if csv_person is None:
-                results = db.execute("select * from person where name like ?", (person.name[:4] + "%", ))
+                results = db.execute("select * from person where name like ?", (person.name[:4] + "%",))
                 results = results.fetchall()
                 results = [self.row_into_person(row) for row in results]
                 print "Database search:", len(results), "possibilities found"
@@ -171,17 +166,22 @@ class Command(BaseCommand):
                 print
 
             if csv_person is not None:
+                skip_person = False
                 person.reg_id = csv_person.reg_id
                 if person.name != csv_person.name:
                     print "CSV Entry found, but names mismatch"
                     self.print_person_summary(person)
                     while True:
                         print "Database:", person.name, " CSV:", csv_person.name
+                        print "  0) Skip"
                         print "  1) Keep Database Name:", person.name
                         print "  2) Use CSV Name:", csv_person.name
                         print "  3) Enter Anew"
                         response = raw_input("Option: ")
-                        if response == "1":
+                        if response == "0":
+                            skip_person = True
+                            break
+                        elif response == "1":
                             break
                         elif response == "2":
                             person.name = csv_person.name
@@ -194,8 +194,10 @@ class Command(BaseCommand):
                         else:
                             print "Invalid Option"
                     print
+                if skip_person:
+                    continue
                 if not (person.address1 or person.address2 or person.city or
-                        person.state or person.country or person.postcode):
+                            person.state or person.country or person.postcode):
                     person.address1 = csv_person.address1
                     person.address2 = csv_person.address2
                     person.city = csv_person.city
@@ -206,7 +208,8 @@ class Command(BaseCommand):
                     person.phone = csv_person.phone
                 if not person.email:
                     person.email = csv_person.email
-                person.save()
+                if not dry_run:
+                    person.save()
 
     def convert_mapping(self, map):
         mapping = []
@@ -231,22 +234,30 @@ class Command(BaseCommand):
 
         db = sqlite3.connect(":memory:")
         db.execute(
-            "create table person ( name text, address1 text, address2 text, city text, state text, country text, postcode text, phone text, email text, reg_id text primary key, comment text )")
-        db.execute("create index table_name_idx on person ( name )")
-        db.execute("create index table_email_idx on person ( email )")
+            "CREATE TABLE person ( name TEXT, address1 TEXT, address2 TEXT, city TEXT, state TEXT, country TEXT, postcode TEXT, phone TEXT, email TEXT, reg_id TEXT PRIMARY KEY, comment TEXT )")
+        db.execute("CREATE INDEX table_name_idx ON person ( name )")
+        db.execute("CREATE INDEX table_email_idx ON person ( email )")
 
         for row in csvfile:
-            values = dict(((t, "") for t in self.targets))
-            for source, target in mapping:
-                value = row.get(source, '')
-                value = value.strip()
-                if value:
-                    if values[target]:
-                        values[target] += " " + value
-                    else:
-                        values[target] = value
-            values['reg_id'] = values['reg_id'].replace(' ', '-')
-            db.execute("insert into person values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )",
-                       tuple(values[t] for t in self.targets))
+            try:
+                values = dict(((t, "") for t in self.targets))
+                for source, target in mapping:
+                    value = row.get(source, '')
+                    value = value.strip()
+                    if value:
+                        if values[target]:
+                            values[target] += " " + value
+                        else:
+                            values[target] = value
+                values['reg_id'] = values['reg_id'].replace(' ', '-')
+            except Exception as exc:
+                print "Problem reading input record. Error: %s Row: %r" % (exc, row)
+                raise
+            try:
+                db.execute("INSERT INTO person VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )",
+                           tuple(values[t] for t in self.targets))
+            except Exception as exc:
+                print "Problem storing data in temporary database. Error: %s Data: %r" % (exc, values)
+                raise
 
         return db
